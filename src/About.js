c841c53e2b1483fc8de28c87f0abd0593f1b1ad0
bzeklaf <44211765+bzeklaf@users.noreply.github.com>
2023-01-13 17:48:42 +0100

    Update About.js

--- src/About.js
+++ src/About.js
@@ -29,7 +29,7 @@ const About = () => {
                <Section animation={"translateX(-200px)"}>
                 <div className="about-text">
                     <div className="heading-large heading-about">What is Black Magic?</div>
-                    <div className="text-large text-large-about">BlackMagic is the finest web3 incubator based in the heart of Europe, Brussels. We take projects in the concept stage, inject the capital in them and guide them all the way to the success. We believe that crypto, blockchain and web3 will change the world and be a force for good.</div>
+                    <div className="text-large text-large-about">BlackMagic is the finest web3 incubator based in the heart of Europe, Brussels. We take projects in the concept stage, inject the capital in them and guide them all the way to success. We believe that crypto, blockchain and web3 will change the world and be a force for good.</div>
                 </div>
                 </Section>
         </div>
